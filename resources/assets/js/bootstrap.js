761e910df81650ed2169ee8601b927aada7d2531
sistemaswebbrasil <adriano.faria@outlook.com.br>
2017-05-26 17:59:48 -0300

    Iniciando

---
+++ resources/assets/js/bootstrap.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import $ from 'jquery'
+import Tether from 'tether'
+import Meta from 'vue-meta'
+import Router from 'vue-router'
+
+import './components'
+import './util/interceptors'
+
+Vue.config.productionTip = false
+
+Vue.use(Router)
+Vue.use(Meta)
+
+window.jQuery = $
+window.Tether = Tether
+require('bootstrap')
